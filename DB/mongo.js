d1a272e275baf89d179a86f263c51668c9b5bcfe
ZainCognitiveCode <xain.graphics69@gmail.com>
2025-03-27 12:25:58 -0800

    made new changes

--- DB/mongo.js
+++ DB/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function mongoDb() {
-    const DB = process.env.DATABASE;
+    const DB = process.env.MONGO_URI;
     mongoose.connect(DB).then(con => {
         console.log('MongoDB Connected Successfully!');
     }).catch(err => console.log('Error Connecting MongoDB: ', err.message));
